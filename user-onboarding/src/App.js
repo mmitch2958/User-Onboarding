c8a94f5fe74a8bb51f38ab31fc2eec3f303c9270
mmitch2958 <mikescottmitchell@outlook.com>
2020-08-20 02:14:15 -0400

    built out form , working and saving new info

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OBForm from './Components/Form'
 
 function App() {
+
+  const [obForm, setOBForm] = useState([
+
+    {
+      id: Date.now,
+      name: '',
+      email: '',
+      pass: '',
+      terms: true,
+    }
+  ]);
+
+  const setNewValue = value => {
+    const newUser = {
+      id: Date.now, 
+      name: value.name,
+      email: value.email, 
+      pass: value.pass, 
+      terms: value.terms,
+    }
+    setOBForm([...obForm, newUser]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>
+          <OBForm setNewValue={setNewValue} />       
+          
+        </p>
+       <div>
+          {obForm.map(user=> {
+              return (
+            <div key = {user.id}> 
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <p>{user.pass}</p>
+            <p>{user.terms}</p>
+            
+            </div>
+          
+              )
+          })
+          
+          }
+          
+       </div>
         </p>
         <a
           className="App-link"
---
+++ user-onboarding/src/Components/Form.js
@@ -0,0 +1,87 @@
+import React, { useState} from 'react'
+
+
+const OBForm = (props) => {
+    const [user, setUser] = useState({
+        id: Date.now,
+        name: '',
+        email: '',
+        pass: '',
+        terms: true,
+    });
+    const [buttonDisabled, setButtonDisabled] = useState(true)
+
+    function dataChange(e) {
+        console.log('what is target.value', e);
+        setUser({ ...user, [e.target.name]: e.target.type==='checkbox' ? e.target.checked: e.target.value});
+    
+    };
+     
+    
+
+    const submitForm = e => {
+        e.preventDefault();
+        console.log('Form Submitted')
+       
+        props.setNewValue(user)
+        setUser({ name: "", email: "", pass: "", terms: true})
+    }
+
+    return (
+        <form onSubmit={submitForm}>
+            <lable htmlFor='id'>
+                ID
+            <input
+                type= {Date}
+                name='id'
+                placeholder='ID'
+                value={Date}
+                onChange={dataChange}
+                />
+            </lable>
+            <lable htmlFor='name'>
+                Name
+            <input
+                type='text'
+                name='name'
+                placeholder='Name'
+                value={user.name}
+                onChange={dataChange}
+                />
+            </lable>
+            <lable htmlFor='email'>
+                Email
+            <input
+                type='text'
+                name='email'
+                placeholder='Website'
+                value={user.email}
+                onChange={dataChange}
+                />
+            </lable>
+            <lable htmlFor='pass'>
+                Pass
+            <input
+                type='text'
+                name='pass'
+                placeholder='Please Enter Your Password'
+                value={user.pass}
+                onChange={dataChange}
+            />
+            </lable>   <lable htmlFor='tos'>
+              Terms Of Service
+            <input
+                    type='checkbox'
+                    name='terms'
+                    checked={user.terms}
+                    onChange={dataChange}
+            />
+            Terms Of Service
+            </lable>
+            
+            <button type='submit'>Submit</button>
+        </form>
+    )   
+
+}
+export default OBForm;
